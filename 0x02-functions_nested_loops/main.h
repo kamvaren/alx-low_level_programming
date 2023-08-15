29d83728e434459ae558b1cc0dee3d6f66cb2e98
Kamvaren <krengqe@gmail.com>
2023-08-15 07:15:11 -0700

    Fix

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,25 +1,56 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
-
+/* _putchar - writes the character c to stdout */
 int _putchar(char c);
+
+/* Task 1 - prints the alphabet, in lowercase, followed by a new line */
 void print_alphabet(void);
+
+/* Task 2 - prints 10 times the alphabet, in lowercase, followed by a new line */
 void print_alphabet_x10(void);
+
+/* Task 3 - checks for lowercase character */
 int _islower(int c);
+
+/* Task 4 - checks for alphabetic character */
 int _isalpha(int c);
+
+/* Task 5 - prints the sign of a number */
 int print_sign(int n);
-int _abs(int n);
+
+/* Task 6 - computes the absolute value of an integer */
+int _abs(int);
+
+/* Task 7 - prints the last digit of a number */
 int print_last_digit(int n);
+
+/* Task 8 - prints every minute of the day of Jack Bauer */
 void jack_bauer(void);
+
+/* Task 9 - prints the 9 times table */
 void times_table(void);
-int add(int a, int b);
+
+/* Task 10 - adds two integers and returns the result */
+int add(int, int);
+
+/* Task 11 - prints all natural numbers from n to 98 */
 void print_to_98(int n);
-void print_times_table(int n); // Task 12
-void print_sum_of_multiples(void); // Task 13
-void print_first_50_fibonacci(void); // Task 14
-void print_even_valued_fibonacci(void); // Task 15
-void print_first_98_fibonacci(void); // Task 16
+
+/* Task 12 - prints the n times table */
+void print_times_table(int n);
+
+/* Task 13 - computes and prints the sum of multiples of 3 or 5 below 1024 */
+void sum_of_multiples(void);
+
+/* Task 14 - prints the first 50 Fibonacci numbers */
+void print_fibonacci(void);
+
+/* Task 15 - finds and prints the sum of even-valued terms in Fibonacci sequence */
+void sum_even_fibonacci(void);
+
+/* Task 16 - prints the first 98 Fibonacci numbers */
+void print_first_98_fibonacci(void);
 
 #endif /* MAIN_H */
 
