8beb2ae9b43787fe0d64226b23a510d1ad2e74fa
Kamvaren <krengqe@gmail.com>
2023-08-15 07:11:19 -0700

    Sine

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,8 @@
-#ifndef ALX_FUNCTIONS_H
-#define ALX_FUNCTIONS_H
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdio.h> // Include necessary standard library headers
+#include <unistd.h>
 
-/* Function prototypes */
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -16,11 +15,11 @@ void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
-void print_times_table(int n);
-int sum_of_multiples(void);
-void print_first_50_fibonacci(void);
-int even_fibonacci_sum(void);
-void print_first_98_fibonacci(void);
+void print_times_table(int n); // Task 12
+void print_sum_of_multiples(void); // Task 13
+void print_first_50_fibonacci(void); // Task 14
+void print_even_valued_fibonacci(void); // Task 15
+void print_first_98_fibonacci(void); // Task 16
 
-#endif /* ALX_FUNCTIONS_H */
+#endif /* MAIN_H */
 
