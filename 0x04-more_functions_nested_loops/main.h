0eed22b487c6965ffa287b02a7df7f146c83f0e1
Kamvaren <krengqe@gmail.com>
2023-08-17 01:17:31 -0700

    Bulk Upload

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,42 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+
+/* Task 0 */
+int _isupper(int c);
+
+/* Task 1 */
+int _isdigit(int c);
+
+/* Task 2 */
+int mul(int a, int b);
+
+/* Task 3 */
+void print_numbers(void);
+
+/* Task 4 */
+void print_most_numbers(void);
+
+/* Task 5 */
+void more_numbers(void);
+
+/* Task 6 */
+void print_line(int n);
+
+/* Task 7 */
+void print_diagonal(int n);
+
+/* Task 8 */
+void print_square(int size);
+
+/* Task 9 */
+void print_triangle(int size);
+
+/* Task 10 */
+void print_fizz_buzz(void);
+
+#endif /* MAIN_H */
+
